daeafefc330e6274ef0f4607d0fdfc5d0ebb6e87
Pseudonym_Tim <36407692+Pseudonym-Tim@users.noreply.github.com>
2023-10-10 06:44:28 -0700

    Uh, okay, maybe this will work?

--- main.js
+++ main.js
@@ -87,9 +87,12 @@ function audioPlayer() {
 }
 
 // remove fragment as much as it can go without adding an entry in browser history:
-window.location.replace("#");
+window.history.replaceState(null, null, ' '); // this will remove the # fragment
+
+/*window.location.replace("#");
 
 // slice off the remaining '#' in HTML5:    
 if (typeof window.history.replaceState == 'function') {
   history.replaceState({}, '', window.location.href.slice(0, -1));
 }
+*/
