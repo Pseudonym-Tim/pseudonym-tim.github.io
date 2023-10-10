d1c37058553af76e770909b77ff277c2590b93ed
Pseudonym_Tim <36407692+Pseudonym-Tim@users.noreply.github.com>
2023-10-10 06:31:14 -0700

    Fragment identifiers should be gone from URL now?

--- main.js
+++ main.js
@@ -86,4 +86,10 @@ function audioPlayer() {
     });
 }
 
-document.location.href = String( document.location.href ).replace( "#/", "" );
+// remove fragment as much as it can go without adding an entry in browser history:
+window.location.replace("#");
+
+// slice off the remaining '#' in HTML5:    
+if (typeof window.history.replaceState == 'function') {
+  history.replaceState({}, '', window.location.href.slice(0, -1));
+}
